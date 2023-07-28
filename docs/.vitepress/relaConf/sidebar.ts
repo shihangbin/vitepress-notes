e7cb2e3328b949eda5b84931aed8b4778ca5250f
冬三九 <18213331550@163.com>
2023-07-28 15:15:38 +0800

    计算机硬件

--- docs/.vitepress/relaConf/sidebar.ts
+++ docs/.vitepress/relaConf/sidebar.ts
@@ -4,9 +4,7 @@ export const sidebar: DefaultTheme.Sidebar = {
 	'/notes/': [
 		{
 			text: 'Vue3',
-			items: [
-				{ text: '简介', link: '/notes/Vue3/01-简介.md' }
-			],
+			items: [{ text: '简介', link: '/notes/Vue3/01-简介.md' }],
 		},
 		// Nodejs
 		{
@@ -37,6 +35,23 @@ export const sidebar: DefaultTheme.Sidebar = {
 				{ text: 'HTML', link: '/article/前端常用英语/html' },
 				{ text: 'CSS', link: '/article/前端常用英语/css' },
 				{ text: 'JavaScript', link: '/article/前端常用英语/js' },
+			],
+		},
+	],
+	'/computer/': [
+		{
+			text: '计算机硬件',
+			// collapsed: true,
+			items: [
+				{ text: '显卡', link: '/computer/计算机硬件/1-显卡' },
+				{ text: 'CUP', link: '/computer/计算机硬件/2-CPU' },
+				{ text: '主板', link: '/computer/计算机硬件/3-主板' },
+				{ text: '内存', link: '/computer/计算机硬件/4-内存' },
+				{ text: '硬盘', link: '/computer/计算机硬件/5-硬盘' },
+				{ text: '电源', link: '/computer/计算机硬件/6-电源' },
+				{ text: '散热器', link: '/computer/计算机硬件/7-散热器' },
+				{ text: '机箱', link: '/computer/计算机硬件/8-机箱' },
+				{ text: '机箱风扇', link: '/computer/计算机硬件/9-机箱风扇' },
 			],
 		},
 	],
